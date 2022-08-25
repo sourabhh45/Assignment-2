108f86b72c6ffb9470b6e21b05007d77d910691c
hp <hp@LAPTOP-TE30B2CR>
2022-08-25 16:05:48 +0530

    my second commit

--- MatrixTranspose/src/anudip/MatrixTranspose.java
+++ MatrixTranspose/src/anudip/MatrixTranspose.java
@@ -1,37 +1,35 @@
 package anudip;
+import java.util.Arrays;
+public class MatrixTranspose {
 
-	// Program to transpose a 3X2 matrix
-
-	public class MatrixTranspose {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] matrix = new  int [][] {{1,2},{4,5},{8,9}};
+		int [][] newmatrix= new int [2][3];
+		int [][] multimatrix = new int[3][1];
 		
-		public static void main(String[] args) {
-			
-			//Given matrix 
-			int mat[][]= {{1,2},{4,5},{8,9}};
-			
-			//another matrix to store transpose of a matrix 
-			int transpose[][]=new int[2][3];//matrix with 2 rows and three columns
-			
-			//Transpose of matrix using for loop
-			for(int i=0;i<2;i++) {
-				for(int j=0;j<3;j++) {
-					transpose[i][j]=mat[j][i];
-				}
+		//1st loop for row of matrix 
+		for(int i = 0;i<matrix.length;i++) {
+			//2nd loop for row of transpose matrix
+			int a = 1;
+			for(int j = 0 ; j<2;j++) {
+				newmatrix[j][i] = matrix[i][j];
+				a = a*  matrix[i][j];
+				
 			}
-			
-			System.out.println("Transpose of matrix:");
-			for(int i=0;i<2;i++){    
-				for(int j=0;j<3;j++){    
-				System.out.print(transpose[i][j]+" "); 
+			multimatrix[i][0] = a; 
 			
 		}
-				System.out.println();
-			}
+		//Transpose matrix
+		System.out.println("Transpose matrix is: " + Arrays.deepToString(newmatrix));
+		
+		// matrix multiplication
+		System.out.println("multiply of matrix is: " + Arrays.deepToString(multimatrix));
+		
+		
+		
 		}
-
-			}
+	
+		}
 		
 	
-		
-
-		
