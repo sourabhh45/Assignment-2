03cafe7f6919f0b0664abcb44ccfeca8d406c838
hp <hp@LAPTOP-TE30B2CR>
2022-08-25 16:11:19 +0530

    my second commit

---
+++ TransposeMatrix/src/anudip/TransposeMatrix.java
@@ -0,0 +1,36 @@
+package anudip;
+
+import java.util.Arrays;
+public class TransposeMatrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] matrix = new  int [][] {{1,2},{4,5},{8,9}};
+		int [][] newmatrix= new int [2][3];
+		int [][] multimatrix = new int[3][1];
+		
+		//1st loop for row of matrix 
+		for(int i = 0;i<matrix.length;i++) {
+			//2nd loop for row of transpose matrix
+			int a = 1;
+			for(int j = 0 ; j<2;j++) {
+				newmatrix[j][i] = matrix[i][j];
+				a = a*  matrix[i][j];
+				
+			}
+			multimatrix[i][0] = a; 
+			
+		}
+		//Transpose matrix
+		System.out.println("Transpose matrix is: " + Arrays.deepToString(newmatrix));
+		
+		// matrix multiplication
+		System.out.println("multiply of matrix is: " + Arrays.deepToString(multimatrix));
+		
+		
+		
+		}
+	
+		}
+		
+	
